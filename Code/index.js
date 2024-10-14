0028d0ce11b0a9b9d5a00b0675cd59271b090c21
sqlwsxo <stasik.buberenko@gmail.com>
2024-10-15 00:23:35 +0300

    update

--- Code/index.js
+++ Code/index.js
@@ -1,2 +1,11 @@
 import HashMap from './functions/HashMap.js'
 
+const hashmap = new HashMap()
+
+hashmap.set('key1', 10)
+hashmap.set('key2', 20)
+hashmap.set('key3', 30)
+
+console.log(hashmap.get('key1'))
+console.log(hashmap.get('key2'))
+console.log(hashmap.get('key3'))
